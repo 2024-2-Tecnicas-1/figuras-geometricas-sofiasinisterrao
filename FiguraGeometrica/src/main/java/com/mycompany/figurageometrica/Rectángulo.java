e15950ef1e6528eebb93ac23c64d8559847299a0
sofiasinisterrao <sofiav.obandos@konradlorenz.edu.co>
2024-10-20 22:41:23 -0500

    nuevo 3.0

--- FiguraGeometrica/src/main/java/com/mycompany/figurageometrica/Rectángulo.java
+++ FiguraGeometrica/src/main/java/com/mycompany/figurageometrica/Rectángulo.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.figurageometrica;
 
-/**
- *
- * @author svos8
- */
-public class Rectángulo {
-    
+package com.mycompany.figurageometrica;
+public class Rectángulo extends FiguraGeometrica{
+ private double lado1;
+ private double lado2;
+ 
+public Rectángulo(String nombre,String color,double lado1,double lado2){
+super(nombre, color );
+this.lado1 = lado1;
+this.lado2 = lado2;
+}
+public double getArea (){
+   return lado1 * lado2;
+}
+public double getPerimetro (){
+  return 2 * (lado1 + lado2);
+}
 }
