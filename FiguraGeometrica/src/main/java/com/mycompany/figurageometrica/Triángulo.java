778696b59cae865b7360947bd0210d2bf54a78ad
sofiasinisterrao <sofiav.obandos@konradlorenz.edu.co>
2024-10-20 22:56:06 -0500

    nuevo 4.0

--- FiguraGeometrica/src/main/java/com/mycompany/figurageometrica/Triángulo.java
+++ FiguraGeometrica/src/main/java/com/mycompany/figurageometrica/Triángulo.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.figurageometrica;
 
-/**
- *
- * @author svos8
- */
+package com.mycompany.figurageometrica;
+public class Triángulo extends FiguraGeometrica {
+    
+
 public class Triángulo {
     
+}
 }
